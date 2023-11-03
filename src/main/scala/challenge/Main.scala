597c2df06f723be1067e8d0812a45cb682ad224b
Slawomir Kania <slawomir.kania1@gmail.com>
2023-11-03 15:50:27 +0100

    running script

--- src/main/scala/challenge/Main.scala
+++ src/main/scala/challenge/Main.scala
@@ -2,6 +2,16 @@ package com.example
 package challenge
 
 object Main {
-  def main(args: Array[String]) =
-    println("example")
+  def main(args: Array[String]) = {
+    val input =
+      """7
+        |6 3
+        |3 8 5
+        |11 2 10 9
+        |""".stripMargin
+
+    val result = MinPath.minPath(input)
+
+    println(s"Minimal path is: $result")
+  }
 }
